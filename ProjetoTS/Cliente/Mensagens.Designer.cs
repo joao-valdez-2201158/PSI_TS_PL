3789d5b5186af16ddd77aebd7cc671e3dcd7b2e9
Eduardo <2202420@my.ipleiria.pt>
2021-05-20 22:51:49 +0100

    New Version 2

---
+++ ProjetoTS/Cliente/Mensagens.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Cliente
+{
+    partial class Mensagens
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbChat = new System.Windows.Forms.TextBox();
+            this.btnChat = new System.Windows.Forms.Button();
+            this.tb2Chat = new System.Windows.Forms.TextBox();
+            this.lbChat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbChat
+            // 
+            this.tbChat.Location = new System.Drawing.Point(9, 335);
+            this.tbChat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tbChat.Multiline = true;
+            this.tbChat.Name = "tbChat";
+            this.tbChat.Size = new System.Drawing.Size(470, 36);
+            this.tbChat.TabIndex = 1;
+            this.tbChat.Text = "Escreva para o BOT";
+            // 
+            // btnChat
+            // 
+            this.btnChat.Location = new System.Drawing.Point(490, 335);
+            this.btnChat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnChat.Name = "btnChat";
+            this.btnChat.Size = new System.Drawing.Size(100, 35);
+            this.btnChat.TabIndex = 5;
+            this.btnChat.Text = "Enviar";
+            this.btnChat.UseVisualStyleBackColor = true;
+            this.btnChat.Click += new System.EventHandler(this.btnChat_Click);
+            // 
+            // tb2Chat
+            // 
+            this.tb2Chat.Location = new System.Drawing.Point(9, 51);
+            this.tb2Chat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tb2Chat.Multiline = true;
+            this.tb2Chat.Name = "tb2Chat";
+            this.tb2Chat.ReadOnly = true;
+            this.tb2Chat.Size = new System.Drawing.Size(583, 279);
+            this.tb2Chat.TabIndex = 6;
+            // 
+            // lbChat
+            // 
+            this.lbChat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbChat.Location = new System.Drawing.Point(-1, 9);
+            this.lbChat.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbChat.Name = "lbChat";
+            this.lbChat.Size = new System.Drawing.Size(613, 25);
+            this.lbChat.TabIndex = 7;
+            this.lbChat.Text = "Chat with the AI";
+            this.lbChat.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Mensagens
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 398);
+            this.Controls.Add(this.lbChat);
+            this.Controls.Add(this.tb2Chat);
+            this.Controls.Add(this.btnChat);
+            this.Controls.Add(this.tbChat);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Mensagens";
+            this.Text = "Mensagens";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbChat;
+        private System.Windows.Forms.Button btnChat;
+        private System.Windows.Forms.TextBox tb2Chat;
+        private System.Windows.Forms.Label lbChat;
+    }
+}
---
+++ ProjetoTS/Cliente/Menu_Principal.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EI.SI;
+
+namespace Cliente
+{
+   
+    public partial class Menu_Principal : Form
+    {
+        NetworkClient Client { get; set; }
+
+        public Menu_Principal()
+        {
+            InitializeComponent();
+        }
+        private void btnAutenticacao_Click(object sender, EventArgs e)
+        {
+            Autenticacao autenticacao = new Autenticacao();
+            autenticacao.Show();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            string portoIntroduzido = tbPort.Text;
+
+            int porto = 10000;
+
+            if (int.TryParse(portoIntroduzido, out porto))
+            {
+
+                this.Client = new NetworkClient(porto); //instanciado com a classe NetworkClient
+                try
+                {
+                    if (this.Client.Connect())
+                    {
+                        this.lbStatus.Text = "Conectado";
+                    }
+                    else
+                    {
+                        this.lbStatus.Text = "Desconectado";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("Introduza porto valido!");
+            }
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            if (this.Client.Close())
+            {
+                this.lbStatus.Text = "Desconectado";
+            }
+            else
+            {
+                this.lbStatus.Text = "Conectado";
+            }
+        }
+        private void btnChat_Click(object sender, EventArgs e)
+        {
+
+            Mensagens mensagens = new Mensagens(this.Client);
+            mensagens.Show();
+        }
+
+        private void btnFicheiros_Click(object sender, EventArgs e)
+        {
+            Ficheiros ficheiros = new Ficheiros(this.Client);
+            ficheiros.Show();
+        }
+
+        private void Menu_Principal_Load(object sender, EventArgs e)
+        {
+            Autenticacao autenticacao = new Autenticacao();
+
+            while(CadastroUtilizador.utilizadorLogado == null){
+                Visible = false;
+                autenticacao.ShowDialog();
+            }
+            Visible = true;
+            lbMenu.Text = "Caro(a) " + CadastroUtilizador.utilizadorLogado.Nome + " Seja Bem Vindo";
+        }
+
+        private void lbMenu_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
